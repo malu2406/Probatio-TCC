7fdf6ea4dffc7672e3628e386fb37d09e33f0f70
Schroeder <129916363+doardaxureder@users.noreply.github.com>
2025-08-02 13:59:56 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./routes');
-const path = require('path');
+const routes = require("./routes");
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public')); // Isso faz com que /css/cadastro.css aponte para public/css/cadastro.css
+app.use(express.static("public")); // Isso faz com que /css/cadastro.css aponte para public/css/cadastro.css
 app.use(routes);
 
 const PORT = process.env.PORT || 3000;
